d3f775c23587f2f7c8016bd9fbe20c95d641cdd7
A Buren <aburen@mun.ca>
2017-09-19 10:58:10 -0230

    Refined code
    * calculate true age
    * identify seals that look like outliers. Asked Alicia to check age,
    length, and weight

--- code/PG_growth.R
+++ code/PG_growth.R
@@ -14,20 +14,20 @@ library(lubridate)
 
 ## read data ----
 setwd('D:/Buren_files/GitHub/PG_Growth/')
-age <- read.csv('data/PG_age.csv', header = T, as.is=T)
-growth <- read.csv('data/PG_growth.csv', header = T, as.is=T)
+age <- read.csv('data/PG_age.csv', header = T, as.is = T)
+growth <- read.csv('data/PG_growth.csv', header = T, as.is = T)
 
 ## merge data sets ----
-data <- merge(growth, age[,c('idsex', 'cohortage')], by='idsex', all.x=T)
+data <- merge(growth, age[,c('idsex', 'cohortage')], by = 'idsex', all.x = T)
 
 ## wrangle data set ----
 # Create dates
 data$dayf <- ifelse(is.na(data$day), 1, data$day)
 data$dates <- ymd(paste(data$year,"-",data$month,"-",data$dayf,sep = ""))
 data$mdates <- ymd(paste('2800',"-",data$month,"-",data$dayf,sep = ""))
-for (i in 1:nrow(data)){
-  if(!is.na(data[i,'dates'])){
-     if(data$month[i] > 7){
+for (i in 1:nrow(data)) {
+  if(!is.na(data[i, 'dates'])) {
+     if(data$month[i] > 7) {
        data$mdates[i] <- data$mdates[i] - years(1)
      }
   }
@@ -35,21 +35,29 @@ for (i in 1:nrow(data)){
 
 # Eliminate foetus, stillborns and starvlings, and YOY
 # There is one seal of unknown sex, also eliminate it
-# age = 99 does not include any individual in the previous categories
-eliminate <- c(0, 91:93)
+# age = 99 does not include any individual in the previous categories. age=99 is unknown
+eliminate <- c(0, 91:93,99)
 data <- data[which(!data$cohortage %in% eliminate),]
 data <- subset(data, sex != 'U')
 subset(data, cohortage == 99)
 
+## calculate true age ----
+## between Sep 1 and Feb 29, substract 1 from cohort age
+## between March 1 and Aug 30, set trueage = cohortage
+data$trueage <- data$cohortage
+t1 <- ymd('2799-09-01')
+t2 <- ymd('2800-02-29')
+data[data$mdates %in% t1:t2, 'trueage'] <- data[data$mdates %in% t1:t2, 'cohortage'] - 1
+
 # create age classes
-data$ageclass <- ifelse(data$cohortage > 7, '8+', data$cohortage)
+data$ageclass <- ifelse(data$trueage > 7, '8+', data$trueage)
 # find outliers in weight by age class
-g <- ggplot(data, aes(ageclass, weight))
-g + geom_violin() 
+g <- ggplot(data[which(!is.na(data$ageclass)),], aes(ageclass, weight))
+g <- g + geom_violin() 
 print(g)
 
-age1 <- subset(data, cohortage == 1)
-age1[which(age1$weight > 50),]   
+unique(data[which(!is.na(data$ageclass)),'cohortage'])
+
 
 ## There is an issue with cohort age! transform to real ages ----
 
@@ -67,8 +75,44 @@ print(p)
 
 p2 <- ggplot(data , aes(as.POSIXct(dates), weight, colour = sex)) 
 p2 <- p2 + geom_point()
-p2 <- p2 + scale_x_datetime(date_breaks="1 months") 
+p2 <- p2 + scale_x_datetime(date_breaks = "1 months") 
 p2 <- p2 + theme_bw()
 p2 <- p2 + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 p2 <- p2 + xlab("Date")
 print(p2)
+
+p3 <- ggplot(data , aes(as.POSIXct(dates), weight, colour = sex)) 
+p3 <- p3 + geom_point()
+p3 <- p3 + scale_x_datetime(date_breaks = "1 months") 
+p3 <- p3 + theme_bw()
+p3 <- p3 + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+p3 <- p3 + xlab("Date")
+p3 <- p3 + facet_wrap(~ageclass, ncol = 1, scales = "free_y")
+print(p3)
+
+
+## check ----
+tocheck <- subset(data, trueage == 0)
+check <- tocheck[which(tocheck$weight > 75),]   
+tocheck <- subset(data, trueage == 1)
+check <- rbind(check, tocheck[which(tocheck$weight > 100),])
+tocheck <- subset(data, trueage == 2)
+check <- rbind(check,tocheck[which(tocheck$weight > 105),] )  
+tocheck <- subset(data, trueage == 3)
+check <- rbind(check,tocheck[which(tocheck$weight > 150),])
+check$issue <- '2heavy4age'
+
+with(data,plot(length,weight))
+check <- rbind(check, cbind(subset(data, length > 140 & weight < 15), issue = '2light4length'))
+check <- rbind(check, cbind(subset(data, length > 110 & length < 120 & weight > 75), issue = '2heavy4length'))
+check <- rbind(check, cbind(subset(data, length > 100 & length < 105 & weight > 57), issue = '2heavy4length'))
+check <- rbind(check, cbind(subset(data, length > 128 & length < 140 & weight > 100), issue = '2heavy4length'))
+check <- rbind(check, cbind(subset(data, length > 139 & length < 141 & weight > 117), issue = '2heavy4length'))
+check <- rbind(check, cbind(subset(data, length > 149 & length < 151 & weight > 145), issue = '2heavy4length'))
+
+rm(tocheck)
+#check[order( check$idsex),]
+check <- check[order( check$idsex),c('idsex', 'nafo', 'year', 'month', 'day', 'weight', 'length', 'pelage', 'cohortage', 'issue')]
+
+write.csv(check, 'output/check_weights.csv', row.names = F)
+
